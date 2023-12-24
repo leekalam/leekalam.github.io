dbde38ff2e909f29bf4d2f3a4de8ede4bdcc885c
leekalam <154661453+leekalam@users.noreply.github.com>
2023-12-24 15:52:54 +0800

    process.php

--- process.php
+++ process.php
@@ -1,8 +1,14 @@
-
+<!doctype html>
 <html>
+<head>
+<meta charset="utf-8">
+<title>无标题文档</title>
+</head>
+
+
 <body>
 	
 	Welcome <?php echo $_POST["name"]; ?><br>
     Your email address is: <?php echo $_POST["content"]; ?>
 </body>
-</html>
+
